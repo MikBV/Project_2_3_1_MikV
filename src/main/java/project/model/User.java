5d7d7f296ce6a1e7402f11a2bb76410adbbfb036
MikBV <bmv00791@gmail.com>
2024-11-15 14:53:36 +0800

    Create Entity User

---
+++ src/main/java/project/model/User.java
@@ -0,0 +1,30 @@
+package project.model;
+
+import javax.persistence.*;
+
+/**
+ * Сущность User
+ */
+@Entity
+public class User {
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    private long id;
+
+    @Column(unique=true, nullable=false, length=64)
+    private String username;
+
+    @Column(nullable = false, length=24)
+    private String password;
+
+    @Column(nullable=false, length=32)
+    private String firstName;
+
+    @Column(nullable=false, length=32)
+    private String lastName;
+
+    public User() {
+    }
+
+
+}
