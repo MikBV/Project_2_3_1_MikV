8387a457398e11d77d5cd6723c69844ba7de1389
MikBV <bmv00791@gmail.com>
2024-11-18 15:00:43 +0800

    Changes in DAO

--- src/main/java/project/DAO/UserDao.java
+++ src/main/java/project/DAO/UserDao.java
@@ -6,9 +6,8 @@ import project.model.User;
 import java.util.List;
 
 public interface UserDao {
-    void saveUser(String name, String lastName, byte age);
+    void addUser(User user);
     void removeUserById(long id);
     List<User> getAllUsers();
-    void cleanUsersTable();
     void updateUser(User user);
 }
--- src/main/java/project/DAO/UserDaoImpl.java
+++ src/main/java/project/DAO/UserDaoImpl.java
@@ -1,36 +1,38 @@
 package project.DAO;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Repository;
 import org.springframework.transaction.annotation.Transactional;
 import project.model.User;
 
 import javax.persistence.EntityManager;
 import java.util.List;
 
+@Repository
 public class UserDaoImpl implements UserDao {
+
+    @Autowired
+    private EntityManager entityManager;
 
     @Override
     @Transactional
-    public void saveUser(String name, String lastName, byte age) {
-
+    public void addUser(User user) {
+        entityManager.persist(user);
     }
 
     @Override
     public void removeUserById(long id) {
-
+        entityManager.remove(entityManager.find(User.class, id));
     }
 
     @Override
     public List<User> getAllUsers() {
-        return List.of();
-    }
-
-    @Override
-    public void cleanUsersTable() {
-
+        String hql = "from User";
+        return entityManager.createQuery(hql).getResultList();
     }
 
     @Override
     public void updateUser(User user) {
-
+        entityManager.merge(user);
     }
 }
