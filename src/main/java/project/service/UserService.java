d6e32abd956707c650a473ce352b6f2b3557c430
MikBV <bmv00791@gmail.com>
2024-11-18 16:14:02 +0800

    Fill service

--- src/main/java/project/service/UserService.java
+++ src/main/java/project/service/UserService.java
@@ -1,4 +1,12 @@
 package project.service;
 
+import project.model.User;
+
+import java.util.List;
+
 public interface UserService {
+    void addUser(User user);
+    void removeUserById(long id);
+    List<User> getAllUsers();
+    void updateUser(User user);
 }
--- src/main/java/project/service/UserServiceClass.java
+++ src/main/java/project/service/UserServiceClass.java
@@ -1,4 +1,35 @@
 package project.service;
 
-public class UserServiceClass {
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+import project.DAO.UserDao;
+import project.model.User;
+
+import java.util.List;
+
+@Service
+public class UserServiceClass implements UserService {
+
+    @Autowired
+    private UserDao userDao;
+
+    @Override
+    public void addUser(User user) {
+        userDao.addUser(user);
+    }
+
+    @Override
+    public void removeUserById(long id) {
+        userDao.removeUserById(id);
+    }
+
+    @Override
+    public List<User> getAllUsers() {
+        return userDao.getAllUsers();
+    }
+
+    @Override
+    public void updateUser(User user) {
+        userDao.updateUser(user);
+    }
 }
