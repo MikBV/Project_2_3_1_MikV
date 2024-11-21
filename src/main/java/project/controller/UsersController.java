7bee62d4371415742286471df8db52fb863460c7
MikBV <bmv00791@gmail.com>
2024-11-22 06:11:56 +0800

    Commit with working web-part

--- src/main/java/project/controller/UsersController.java
+++ src/main/java/project/controller/UsersController.java
@@ -1,5 +1,6 @@
 package project.controller;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -9,9 +10,13 @@ import project.service.UserServiceImpl;
 
 @Controller
 public class UsersController {
+
+    @Autowired
+    private UserService userService;
+
+
     @GetMapping(value = "/users")
     public String index(Model model) {
-        UserService userService = new UserServiceImpl();
         model.addAttribute("users", userService.getAllUsers());
         return "users";
     }
